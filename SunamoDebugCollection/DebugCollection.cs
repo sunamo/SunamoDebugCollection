1cd713a2457e4e450437388f36436708d7498fdb
Radek Jancik <radek.jancik@usys.cz>
2026-01-01 14:55:44 +0100

    feat: improved code quality

--- SunamoDebugCollection/DebugCollection.cs
+++ SunamoDebugCollection/DebugCollection.cs
@@ -1,49 +1,60 @@
 namespace SunamoDebugCollection;
 
 /// <summary>
-///     Can be use also in production
+/// A debug-friendly collection that can be used in both development and production environments.
+/// Provides functionality to prevent adding specific items to the collection.
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">The type of elements in the collection.</typeparam>
 public class DebugCollection<T> : List<T>
 {
+    /// <summary>
+    /// Gets or sets the list of items that should not be allowed to be added to this collection.
+    /// </summary>
     public List<T> DontAllow { get; set; } = new();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DebugCollection{T}"/> class.
+    /// </summary>
     public DebugCollection()
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DebugCollection{T}"/> class with items from an existing collection.
+    /// </summary>
+    /// <param name="items">The collection whose elements are copied to the new collection.</param>
     public DebugCollection(IList<T> items) : base(items)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DebugCollection{T}"/> class with a specified capacity.
+    /// </summary>
+    /// <param name="count">The initial capacity of the collection.</param>
     public DebugCollection(int count) : base(count)
     {
     }
 
-    public new T this[int i]
+    /// <summary>
+    /// Gets or sets the element at the specified index.
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to get or set.</param>
+    /// <returns>The element at the specified index.</returns>
+    public new T this[int index]
     {
-        get => base[i];
-        set
-        {
-            if (char.IsLower(value.ToString()[0]))
-            {
-            }
-
-            base[i] = value;
-        }
+        get => base[index];
+        set => base[index] = value;
     }
 
-    public new void Add(T item)
+    /// <summary>
+    /// Adds an item to the collection if it is not in the DontAllow list.
+    /// </summary>
+    /// <param name="value">The value to add to the collection.</param>
+    public new void Add(T value)
     {
-        if (DontAllow.Contains(item))
+        if (!DontAllow.Contains(value))
         {
-#if DEBUG
-            //////DebugLogger.Break();
-#endif
-        }
-        else
-        {
-            base.Add(item);
+            base.Add(value);
         }
     }
 }
--- SunamoDebugCollection/DebugLongCollection.cs
+++ SunamoDebugCollection/DebugLongCollection.cs
@@ -1,7 +1,15 @@
+// variables names: ok
 namespace SunamoDebugCollection;
 
+/// <summary>
+/// A specialized debug collection for long integer values with statistical tracking capabilities.
+/// </summary>
 public class DebugLongCollection : DebugCollection<long>
 {
+    /// <summary>
+    /// Adds a long value to the collection and tracks statistics every 100 items in debug mode.
+    /// </summary>
+    /// <param name="value">The long value to add to the collection.</param>
     public new void Add(long value)
     {
         base.Add(value);
